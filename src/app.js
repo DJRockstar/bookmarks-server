b16de23b26e13dafa6f941806870f54e5b404252
DJRockstar <digvijays18@gmail.com>
2019-10-27 17:25:54 -0700

    updated auth token logic

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
 
-  if (!authToken || apiToken !== authToken) {
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     logger.error(`Unauthorized request to path ${req.path}`);
     return res.status(401).json({ error: "Unauthorized Request" });
   }
